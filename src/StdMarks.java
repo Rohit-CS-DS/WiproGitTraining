7ab6a94f7f4e3feb63e4636f01fe905c8428c73a
Rohit-CS-DS <vishwakarma1106rohit@hmail.com>
2025-01-23 18:55:49 +0530

    added

---
+++ src/StdMarks.java
@@ -0,0 +1,10 @@
+
+public class StdMarks {
+
+	public static void main(String[] args) {
+		int english=42,hindi=43,science=45;
+		System.out.println("english ="+english+", Hindi ="+hindi+", Science ="+science);
+
+	}
+
+}
